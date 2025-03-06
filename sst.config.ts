159107863500823bf5b6154eea4e120e096b05b4
Prime Again <the.primeagain@gmail.com>
2025-03-06 10:07:58 -0700

    feat: almost just committed my env

--- sst.config.ts
+++ sst.config.ts
@@ -1,5 +1,7 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
+import { profile } from "console";
+
 export default $config({
     app(input) {
         return {
@@ -7,13 +9,19 @@ export default $config({
             removal: input?.stage === "production" ? "retain" : "remove",
             protect: ["production"].includes(input?.stage),
             home: "aws",
+            providers: { cloudflare: "5.49.1" },
         };
     },
     async run() {
-        const vpc = new sst.aws.Vpc("Vpc", {})
+        const vpc = new sst.aws.Vpc("Vpc", {});
         const cluster = new sst.aws.Cluster("Cluster", {
             vpc,
-        })
+        });
+
+        const prod = $app.stage === "production"
+        const domain = prod
+            ? "vimguys.theprimeagen.com"
+            : `${$app.stage}.vimguys.theprimeagen.com`;
 
         const ap = new sst.aws.Service("AuthProxy", {
             // TODO look at usage logs
@@ -22,25 +30,31 @@ export default $config({
                 max: 16,
             },
             image: {
-                context: "./auth-proxy/"
+                context: "./auth-proxy/",
             },
             wait: true,
             cluster,
             loadBalancer: {
+                domain: {
+                    name: domain,
+                    dns: sst.cloudflare.dns({
+                        proxy: prod,
+                    }),
+                },
                 rules: [
                     { listen: "80/http", forward: "8080/http" },
-                    //{ listen: "443/https", forward: "8080/http" },
-                ]
-            }
-        })
-
+                    { listen: "443/https", forward: "8080/http" },
+                ],
+            },
+        });
         const vg = new sst.aws.Service("VimGuys", {
             // TODO set these to make it so i can run ~1000 games
             image: {
-                context: "./vim-guys/"
+                context: "./vim-guys/",
             },
             wait: true,
             cluster,
-        })
+        });
     },
 });
+
